1df52da1306cb44ef0afa137190f4d276e981f11
Luka Lüdicke <kluedicke@gmail.com>
2021-01-13 19:15:02 +0100

    Add employee scaffolds

---
+++ db/migrate/20210113181144_create_employees.rb
@@ -0,0 +1,11 @@
+class CreateEmployees < ActiveRecord::Migration[6.1]
+  def change
+    create_table :employees do |t|
+      t.string :nick_name
+      t.string :department
+      t.time :deleted_at
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/employees.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :employee do
+    nick_name { "MyString" }
+    department { "MyString" }
+    deleted_at { "2021-01-13 19:11:44" }
+  end
+end
