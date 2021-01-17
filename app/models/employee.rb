650a391691e696be0868e58da760123fbeff91fe
Luka Lüdicke <kluedicke@gmail.com>
2021-01-17 20:28:53 +0100

    Extract department strings to constant

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Employee < ApplicationRecord
+  DEPARTMENTS = %w[operations sales marketing risk management finance HR development data].freeze
+
   validates_presence_of :nick_name
   validates_presence_of :department
 
